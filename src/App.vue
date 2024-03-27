8c168721b3e9b0a7e70f59defbe2c043895aca4d
Philipp Scheffler <p.scheffler@ad-media.de>
2024-03-27 10:46:25 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-list nav>
+        <v-list-item title="Item 1" />
+        <v-list-item title="Item 2" />
+        <template #append>
+          <v-list-item title="Append" />
+        </template>
+      </v-list>
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
